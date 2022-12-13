94ff4b92b37e51cb7aa7b99c6febc68cc89f4b2b
Konstantin Pivnov <vaproloff@ya.ru>
2022-12-14 02:18:40 +0300

    added validator module and constants

---
+++ validator.py
@@ -0,0 +1,12 @@
+import re
+import const
+
+def validate_num(pattern, num):
+    if re.fullmatch(pattern, num):
+        return True
+    return False
+
+print(validate_num(const.RATIONAL_PATTERN, '-42/88'))
+print(validate_num(const.RATIONAL_PATTERN, '-4.2/+88'))
+print(validate_num(const.COMPLEX_PATTERN, '3 + 10j'))
+print(validate_num(const.COMPLEX_PATTERN, '-3.2-223.231j'))
