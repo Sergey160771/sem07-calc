5d6d247c83d90a862c1416453f291fc73add2b79
Konstantin Pivnov <vaproloff@ya.ru>
2022-12-13 19:11:40 +0300

    rename function in calc_rational module

--- calc_rational.py
+++ calc_rational.py
@@ -1,6 +1,6 @@
 from fractions import Fraction
 
-def count(frac1, frac2, sign):
+def calc_value(frac1, frac2, sign):
     if sign == '+':
         result = Fraction(frac1) + Fraction(frac2)
     elif sign == '-':
