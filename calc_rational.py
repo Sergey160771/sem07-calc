0a16843781c67e53876c4cfd0dba81122716e2d7
Sergy <9058591133@mail.ru>
2022-12-13 11:48:11 +0500

    ациональные числa

--- calc_rational.py
+++ calc_rational.py
@@ -1,6 +1,12 @@
 from fractions import Fraction
 
-def calc_value(frac1, frac2, sign):
+def count(frac1, frac2, sign):
     if sign == '+':
         result = Fraction(frac1) + Fraction(frac2)
+    elif sign == '-':
+        result = Fraction(frac1) - Fraction(frac2)
+    elif sign == '*':
+        result = Fraction(frac1) * Fraction(frac2)
+    else:
+        result = Fraction(frac1) / Fraction(frac2)  
     return result
