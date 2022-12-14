b06b7276543ada5f1bd0213178ecebb4ec19e827
Konstantin Pivnov <vaproloff@ya.ru>
2022-12-14 12:36:37 +0300

    added some corrections to view, logger and controller

--- controller.py
+++ controller.py
@@ -4,7 +4,7 @@ import calc_complex
 
 def calc():
     mode = view.get_mode()
-    num1, num2, sign = view.get_data()
+    num1, sign, num2 = view.get_data()
     if mode == 1:
         result = calc_rational.calc_value(num1, num2, sign)
     else:
--- view.py
+++ view.py
@@ -1,25 +1,28 @@
 import logger
 
+num1 = ''
+num2 = ''
+sign = ''
+
 def get_mode():
-    print('С какими числами вы планируете работать?\n')
-    tmp = input('Введите цифру 0, если с комплексными, и цифру 1, если с рациональными:\n')
-    if tmp == '1':
-        return 1
-    elif tmp == '0':
-        return 0
+    print('С какими числами вы планируете работать?')
+    mode = input('1. Рациональные\n2. Комплексные\nВведите 1 или 2: ')
+    if mode == '1' or mode == '2':
+        return int(mode)
     else:
-        print('Ошибка! Введите 1 или 0.')
+        print('Ошибка! Введите 1 или 2.')
 
 def get_data():
     global num1
     global sign
     global num2
-    num1 = input('\nВведите первое число:\n')
-    sign = input('Введите операцию (+, -, *, /):\n')
-    num2 = input('Введите второе число:\n')
+    num1 = input('\nВведите первое число: ')
+    sign = input('Введите операцию (+, -, *, /): ')
+    num2 = input('Введите второе число: ')
 
     return num1, sign, num2
 
 def view_data(result):
-    logger.add_to_log(f'Результат выражения {num1} {sign} {num2} = {result}\n')
-    print(f'\nРезультат выражения {num1} {sign} {num2} = {result}')
+    res_str = f'{num1} {sign} {num2} = {result}'
+    logger.add_to_log(res_str)
+    print(res_str)
