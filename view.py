7584208f00b621a3492c32e039cc4e45c2ca9725
Ruslan Karimov <ru-ka-@mail.ru>
2022-12-14 13:20:58 +0500

    added branch view

--- view.py
+++ view.py
@@ -1,14 +1,25 @@
 import logger
 
 def get_mode():
-
-    return 1
+    print('С какими числами вы планируете работать?\n')
+    tmp = input('Введите цифру 0, если с комплексными, и цифру 1, если с рациональными:\n')
+    if tmp == '1':
+        return 1
+    elif tmp == '0':
+        return 0
+    else:
+        print('Ошибка! Введите 1 или 0.')
 
 def get_data():
+    global num1
+    global sign
+    global num2
+    num1 = input('\nВведите первое число:\n')
+    sign = input('Введите операцию (+, -, *, /):\n')
+    num2 = input('Введите второе число:\n')
 
-    return '1', '2', '+'
+    return num1, sign, num2
 
 def view_data(result):
-
-    logger.add_to_log('result string')
-    print('result string')
+    logger.add_to_log(f'Результат выражения {num1} {sign} {num2} = {result}\n')
+    print(f'\nРезультат выражения {num1} {sign} {num2} = {result}')
